579894897eb69ce7d721230d5ad4e599640ede45
Zaheal <lepskiezakhar@gmail.com>
2022-08-31 16:05:15 +0300

    live
    live

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from game_of_life import GameOfLife
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    GameOfLife(25, 25)
+    return render_template('index.html')
+
+
+@app.route('/live')
+def live():
+    game = GameOfLife()
+    game = game.generate_universe()
+    return render_template("live.html",
+                           game=game)
+
+
+if __name__ == '__main__':
+    app.run()
